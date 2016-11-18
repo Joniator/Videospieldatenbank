87089b3eb3450eae8b7a2c99654b332b7d679190
JonnyB <jonathanboeckel1996@gmail.com>
2016-11-18 12:12:56 +0100

    Base
    Leeres Window und grundlegende MySQL-Connection

---
+++ Videospieldatenbank/Videospieldatenbank/DatabaseInteraction.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using MySql.Data.MySqlClient;
+
+namespace Videospieldatenbank
+{
+    class DatabaseInteraction
+    {
+        private MySqlConnection _connection;
+
+        public bool Connected => _connection.State == ConnectionState.Open;
+
+        public DatabaseInteraction(string server, int port, string databaseName, string userId)
+        {
+            string connectionString = $"Server={server};" +
+                                      $"port={port};" +
+                                      $"Database={databaseName};" +
+                                      $"Uid={userId};";
+
+            _connection = new MySqlConnection(connectionString);
+            _connection.Open();
+        }
+    }
+}
