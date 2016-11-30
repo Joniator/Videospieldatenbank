b1f0a7b0247105a452e800c7be762660018bb0a5
JonnyB <jonathanboeckel1996@gmail.com>
2016-11-30 22:41:26 +0100

    Database.sql Semicolons hinzugefügt...

--- Videospieldatenbank/Videospieldatenbank/Database.sql
+++ Videospieldatenbank/Videospieldatenbank/Database.sql
@@ -8,7 +8,7 @@
 	plattforms VARCHAR(512),
 	genre VARCHAR(512),
 	rating INTEGER
-)
+);
 
 CREATE TABLE user(
 	PRIMARY KEY (id),
@@ -16,17 +16,17 @@ CREATE TABLE user(
 	name VARCHAR(128),
 	password VARCHAR(128),
 	online BOOLEAN
-)
+);
 
 CREATE TABLE friends(
 	PRIMARY KEY (user_ID, friend_ID),
 	user_ID INTEGER,
 	friend_ID INTEGER
-)
+);
 
 CREATE TABLE playtime(
 PRIMARY KEY (user_ID, igdb_url),
 	user_ID INTEGER,
 	igdb_url VARCHAR(512),
 	playtime TIME 
-)
+);
