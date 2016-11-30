928980e47211a7cd76a3ae13cdb331429792fb93
JonnyB <jonathanboeckel1996@gmail.com>
2016-11-30 22:39:28 +0100

    DatabaseConnector hinzugefügt

---
+++ Videospieldatenbank/Videospieldatenbank/DatabaseConnector.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace Videospieldatenbank
+{
+    public class DatabaseConnector
+    {
+        private readonly MySqlConnection _mySqlConnection = new MySqlConnection();
+        public bool Connected => _mySqlConnection.Ping();
+
+        /// <exception cref="MySqlException">Verbindung zum Server fehlgeschlagen.</exception>
+        public DatabaseConnector()
+        {
+            MySqlConnectionStringBuilder builder = new MySqlConnectionStringBuilder()
+            {
+                Server = "tazed.tk",
+                UserID = "igdb",
+                Password = "QhwnUzLpyWbchfUF",
+                Database = "igdb"
+            };
+            _mySqlConnection.ConnectionString = builder.ToString();
+            _mySqlConnection.Open();
+        }
+    }
+}
