79bffb723d30a2610aa2f93ee494213118e45db3
JonnyB <jonathanboeckel1996@gmail.com>
2016-12-09 10:14:27 +0100

    Dataenbankverbindnungen

--- Videospieldatenbank/Videospieldatenbank/Tests/UserTest.cs
+++ Videospieldatenbank/Videospieldatenbank/Tests/UserTest.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using Videospieldatenbank.Database;
+using Videospieldatenbank.Utils;
 
 namespace Videospieldatenbank.Tests
 {
@@ -14,9 +16,25 @@ namespace Videospieldatenbank.Tests
             try
             {
                 UserDatabaseConnector udb = new UserDatabaseConnector();
-                return udb.Register("TestUser", "TestPassword") && udb.Login("TestUser", "TestPassword") && udb.Logout();
+                udb.DeleteUser("TestUser", "TestPassword");
+                using (WebClient client = new WebClient())
+                {
+                    byte[] image = client.DownloadData("http://www.skringers.com/wp-content/uploads/Beautiful-Landscape-Wallpapers-Island-150x150.jpg");
+
+
+                    string iget = udb.GetProfilePicture("TestUser").ByteArrayToString();
+                    string iref = image.ByteArrayToString();
+                    bool startsWith = iget.StartsWith(iref);
+
+                    return udb.Register("TestUser", "TestPassword") && 
+                        udb.Login("TestUser", "TestPassword") && 
+                        udb.SetProfilePicture(image) && 
+                        udb.GetProfilePicture("TestUser").ByteArrayToString().StartsWith(image.ByteArrayToString()) &&
+                        udb.Logout() && 
+                        udb.DeleteUser("TestUser", "TestPassword");
+                }
             }
-            catch
+            catch (Exception e)
             {
                 return false;
             }
--- Videospieldatenbank/Videospieldatenbank/Utils/Utils.cs
+++ Videospieldatenbank/Videospieldatenbank/Utils/Utils.cs
@@ -8,7 +8,12 @@ namespace Videospieldatenbank.Utils
 {
     public static class ImageUtils
     {
-        public static byte[] StringToByteArray(string source)
+        /// <summary>
+        /// Konvertiert einen String in ein Bytearray.
+        /// </summary>
+        /// <param name="source">Der zu konvertierende String.</param>
+        /// <returns>Bytearray</returns>
+        public static byte[] ToByteArray(this string source)
         {
             byte[] byteArray = new byte[source.Length];
             for (int i = 0; i < source.Length; i++)
@@ -18,14 +23,35 @@ namespace Videospieldatenbank.Utils
             return byteArray;
         }
 
-        public static string ByteArrayToString(byte[] source)
+        /// <summary>
+        /// Konvertiert einen Bytearray in einen String.
+        /// </summary>
+        /// <param name="array"></param>
+        /// <returns></returns>
+        public static string ByteArrayToString(this byte[] array)
         {
             StringBuilder stringBuilder = new StringBuilder(65536);
-            foreach (byte b in source)
+            foreach (byte b in array)
             {
                 stringBuilder.Append((char)b);
             }
             return stringBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Konvertiert einen Stringarray in einen String.
+        /// </summary>
+        /// <param name="array">Das zu konvertierende Array.</param>
+        /// <param name="delimiter">Das Zeichen mit dem die Einträge getrennt werden.</param>
+        /// <returns>String</returns>
+        public static string StringArrayToString(this string[] array, string delimiter)
+        {
+            string result = "";
+            foreach (string s in array)
+            {
+                result += s + delimiter;
+            }
+            return result;
         }
     }
 }
